cb5da6cd9ef60a417116b3140ac595ccff3dbb93
JunSeong_Park <ttotto0608@naver.com>
2023-03-21 15:29:39 +0900

    first commit

---
+++ src/main/java/com/example/commons_exepractice/CommonsExePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.commons_exepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonsExePracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonsExePracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/commons_exepractice/CommonsExePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commons_exepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonsExePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
