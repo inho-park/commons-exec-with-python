c02b4d22e378f3497c984f9371ba771a261f99af
JunSeong_Park <ttotto0608@naver.com>
2023-03-21 15:36:09 +0900

    execute python with os command in java

---
+++ src/main/java/com/example/commons_exepractice/MainController.java
@@ -0,0 +1,44 @@
+package com.example.commons_exepractice;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.PumpStreamHandler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+@RestController
+public class MainController {
+    @PostMapping()
+    public ResponseEntity testPython() {
+        String [] command = new String[4];
+        command[0] = "python";
+        command[1] = "D://test.py";
+        command[2] = "10";
+        command[3] = "20";
+        try {
+            return ResponseEntity.ok().body(execPython(command));
+        } catch(Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    public static String execPython(String[] command) throws IOException, InterruptedException {
+        CommandLine commandLine = CommandLine.parse(command[0]);
+        for (int i = 1, n = command.length; i<n; i++) {
+            commandLine.addArgument(command[i]);
+        }
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PumpStreamHandler pumpStreamHandler = new PumpStreamHandler(outputStream);
+        DefaultExecutor executor = new DefaultExecutor();
+        executor.setStreamHandler(pumpStreamHandler);
+        int result = executor.execute(commandLine);
+        System.out.println("result : " +  result);
+        System.out.println("output : " + outputStream.toString());
+        return outputStream.toString();
+    }
+}
